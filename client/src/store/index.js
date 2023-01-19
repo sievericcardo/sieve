fdb06f864a1623697eb4e05a41e9a54228464628
Riccardo Sieve <sievericcardo@gmail.com>
2023-01-19 16:46:49 +0100

    Combined stores

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducers/reducer';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 const persister = 'Free';
 
 export { store, persister };
