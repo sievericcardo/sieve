bda76c270cb2a0b2648bc9bde89d7da57b364411
Riccardo Sieve <sievericcardo@gmail.com>
2023-01-26 00:11:07 +0100

    Update authHooks.js
    modified storage handling

--- client/src/hooks/authHooks.js
+++ client/src/hooks/authHooks.js
@@ -5,9 +5,10 @@ import { url } from "../api/index";
 export const signUp = async (user) => {
   try {
     const response = await axios.post(`${url}/signup`, user);
-    localStorage.setItem("token", response.data);
+    localStorage.setItem("token", response.data.token);
     localStorage.setItem("user", user.name);
-    return response;
+    localStorage.setItem("avatar", response.data.image);
+    return true;
   } catch (error) {
     toast.error(error.response?.data, {
       position: toast.POSITION.BOTTOM_RIGHT,
@@ -18,8 +19,9 @@ export const signUp = async (user) => {
 export const signIn = async (user) => {
   try {
     const response = await axios.post(`${url}/signin`, user);
-    localStorage.setItem("token", response.data);
+    localStorage.setItem("token", response.data.token);
     localStorage.setItem("user", user.name);
+    localStorage.setItem("avatar", response.data.image);
     return;
   } catch (error) {
     toast.error(error.response?.data, {
