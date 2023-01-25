36ce7b82186b6209a9755d558aaf1a265322da08
Riccardo Sieve <sievericcardo@gmail.com>
2023-01-26 00:11:15 +0100

    Create projectHooks.js
    added hooks for projects

---
+++ client/src/hooks/projectHooks.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { url, setHeaders } from "../api/index";
+
+export const getProjects = async () => {
+  try {
+    const projects = await axios.get(`${url}/projects`);
+    return projects.data;
+  } catch (error) {
+    toast.error(error.response?.data, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+  }
+}
+
+export const getProject = async (id) => {
+  try {
+    const project = await axios.get(`${url}/projects/${id}`);
+
+    return project.data;
+  } catch (error) {
+    toast.error(error.response?.data, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+  }
+}
+
+export const addProject = async (project) => {
+  try {
+    const addedProject = await axios.post(
+      `${url}/projects`,
+      project,
+      setHeaders()
+    );
+    return true;
+  } catch (error) {
+    toast.error(error.response?.data, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+  }
+}
+
+export const updateProject = async (updatedProject, id) => {
+  try {
+    const project = await axios.put(
+      `${url}/projects/${id}`,
+      updatedProject,
+      setHeaders()
+    );
+    return project.data;
+  } catch (error) {
+    toast.error(error.response?.data, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+  }
+}
+
+export const deleteProject = async (id) => {
+  try {
+    const project = await axios.delete(`${url}/projects/${id}`, setHeaders());
+    return project.data;
+  } catch (error) {
+    toast.error(error.response?.data, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+  }
+}
