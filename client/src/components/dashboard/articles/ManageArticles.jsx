462335d2692c14158750c3dc02bc2b859858f4b1
Riccardo Sieve <sievericcardo@gmail.com>
2023-01-26 21:24:28 +0100

    Update ManageArticles.jsx
    removed unused redux

--- client/src/components/dashboard/articles/ManageArticles.jsx
+++ client/src/components/dashboard/articles/ManageArticles.jsx
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
 
 import AddArticle from '../articles/AddArticle';
 import ListArticles from '../articles/ListArticles';
 
 const ManageArticles = () => {
-    const auth = useSelector(state => state.auth);
 
     const [ article, setArticle ] = useState({
         name: "",
         body: "",
         author: "Riccardo",
     });
-
-    // if(!auth._id) {
-    //     return <Navigate to="/signin" />
-    // }
 
     return (
         <>
