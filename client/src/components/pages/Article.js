e0efee174d663f3aa465c63c02604d16f0fc1e5e
Riccardo Sieve <sievericcardo@gmail.com>
2023-01-27 20:42:04 +0100

    Create Article.js
    Create page for single Article

---
+++ client/src/components/pages/Article.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import { AllInclusive } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+import ReactMarkdown from 'react-markdown';
+
+import { getArticle } from '../../hooks/articleHooks';
+
+const Root = styled('div')({
+  backgroundColor: 'rgba(50, 115, 220, 0.3)',
+  marginTop: '1.4rem',
+  marginBottom: '1rem',
+  padding: '1rem',
+});
+
+const Article = () => {
+  const { id } = useParams();
+  const [article, setArticle] = useState({});
+
+  useEffect(() => {
+    getArticle(id).then((data) => {
+      setArticle(data);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Root>
+      <Typography variant="h4"><AllInclusive /> {article.name}</Typography>
+      <Typography variant="body2">
+        {article.body}
+      </Typography>
+      <ReactMarkdown>{ decodeURIComponent(escape(article.body)) }</ReactMarkdown>
+    </Root>
+  );
+};
+
+export default Article;
