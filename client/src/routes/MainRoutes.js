94d6cc2bb7f1e580c484b385eecbea70a927ca92
Riccardo Sieve <sievericcardo@gmail.com>
2023-01-27 20:41:44 +0100

    Update MainRoutes.js
    Added routes for single articles and projects

--- client/src/routes/MainRoutes.js
+++ client/src/routes/MainRoutes.js
@@ -4,6 +4,8 @@ import Loadable from '../components/ui/Loadable';
 import MainLayout from '../layout/MainLayout';
 
 const Home = Loadable(lazy(() => import('../components/pages/Home')));
+const Project = Loadable(lazy(() => import('../components/pages/Project')));
+const Article = Loadable(lazy(() => import('../components/pages/Article')));
 // const Writeup = Loadable(lazy(() => import('../components/pages/Writeup')));
 // const Projects = Loadable(lazy(() => import('../components/pages/Projects')));
 // const DisplayWriteups = Loadable(lazy(() => import('../components/pages/DisplayWriteups')));
@@ -16,6 +18,14 @@ const MainRoutes = {
       path: '/',
       element: <Home />
     },
+    {
+      path: '/project/:id',
+      element: <Project />
+    },
+    {
+      path: '/article/:id',
+      element: <Article />
+    }
     // {
     //   path: 'writeups',
     //   element: <DisplayWriteups />
