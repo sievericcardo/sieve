e7397e41f23766f9901c08013bc5c173f69fb7f9
Riccardo Sieve <sievericcardo@gmail.com>
2023-07-27 11:45:47 +0200

    Update Home.jsx
    inserted initial block into a div

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -43,21 +43,23 @@ const Home = () => {
     <Root className={ classes.base }>
       <Banner childToParent={[ animeState ? animeBanner : banner, "Home page banner"]}/>
       <div className='homeMainContent'>
-        <Typography variant="h1" className='homeTitle'>
-          My personal portfolio
-        </Typography>
-        <Typography variant="body1">
-          Since I was very little I had a passion for technology, playing games and trying to figure out how they worked and how they were created; I can still remember the hours playing Final Fantasy X and trying to understand how that characters could move, how the various event were handled and how everything was so perfectly scripted.
-          During high school I started learning C with the Kernighan Ritchie manual, and learning a bit of C++ and Java as well. During the last two years I also started learning HTML and Blender for 3D art (I was still mesmerized with the beauty of Final Fantasy X and I wanted to be able to create that).
-        </Typography>
-        <p>&nbsp;</p>
-        <Typography variant="body1">
-          After an initial study in computer engineering, I decided to move into computer science to have the chance to work more on programming projects and have a higher knowledge in languages, algorithms, compilers and the functionalities of the computer architecture.
-        </Typography>
-        <p>&nbsp;</p>
-        <Typography variant="body1">
-          Even now that passion still lies and I keep learning and studying to improve and I constantly look for challenges and improvement possibilities.
-        </Typography>
+        <div className='introBlock'>
+          <Typography variant="h1" className='homeTitle'>
+            My personal portfolio
+          </Typography>
+          <Typography variant="body1">
+            Since I was very little I had a passion for technology, playing games and trying to figure out how they worked and how they were created; I can still remember the hours playing Final Fantasy X and trying to understand how that characters could move, how the various event were handled and how everything was so perfectly scripted.
+            During high school I started learning C with the Kernighan Ritchie manual, and learning a bit of C++ and Java as well. During the last two years I also started learning HTML and Blender for 3D art (I was still mesmerized with the beauty of Final Fantasy X and I wanted to be able to create that).
+          </Typography>
+          <p>&nbsp;</p>
+          <Typography variant="body1">
+            After an initial study in computer engineering, I decided to move into computer science to have the chance to work more on programming projects and have a higher knowledge in languages, algorithms, compilers and the functionalities of the computer architecture.
+          </Typography>
+          <p>&nbsp;</p>
+          <Typography variant="body1">
+            Even now that passion still lies and I keep learning and studying to improve and I constantly look for challenges and improvement possibilities.
+          </Typography>
+        </div>
         <Block>
           <Typography variant="h3" className='subTitle'>
             Projects
