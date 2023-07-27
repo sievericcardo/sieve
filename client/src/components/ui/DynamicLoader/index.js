b481872567213c7fda08ea2bae08077143a6b339
Riccardo Sieve <sievericcardo@gmail.com>
2023-07-27 11:48:41 +0200

    inserted boilerplate for loader

---
+++ client/src/components/ui/DynamicLoader/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Loader.css'; // Create this CSS file in the same folder
+
+const DynamicLoader = () => {
+  return <div className="loader"></div>;
+};
+
+export default DynamicLoader;
