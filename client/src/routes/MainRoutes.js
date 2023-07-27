1b491bb0edf5c7f2785c1857edf0c059af9afc7a
Riccardo Sieve <sievericcardo@gmail.com>
2023-07-27 11:31:05 +0200

    Update MainRoutes.js
    changed main routes to add check on anime state

--- client/src/routes/MainRoutes.js
+++ client/src/routes/MainRoutes.js
@@ -2,28 +2,33 @@ import { lazy } from 'react';
 
 import Loadable from '../components/ui/Loadable';
 import MainLayout from '../layout/MainLayout';
+import AnimeLayout from '../layout/AnimeLayout';
 
 const Home = Loadable(lazy(() => import('../components/pages/Home')));
 const Project = Loadable(lazy(() => import('../components/pages/Project')));
 const Article = Loadable(lazy(() => import('../components/pages/Article')));
 
-const MainRoutes = {
-  path: '/',
-  element: <MainLayout />,
-  children: [
-    {
-      path: '/',
-      element: <Home />
-    },
-    {
-      path: '/project/:id',
-      element: <Project />
-    },
-    {
-      path: '/article/:id',
-      element: <Article />
-    }
-  ]
+const MainRoutes = (animeState) => {
+  const layoutElement = animeState ? <AnimeLayout /> : <MainLayout />;
+
+  return {
+    path: '/',
+    element: layoutElement,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/project/:id',
+        element: <Project />
+      },
+      {
+        path: '/article/:id',
+        element: <Article />
+      }
+    ]
+  }
 };
 
 export default MainRoutes;
